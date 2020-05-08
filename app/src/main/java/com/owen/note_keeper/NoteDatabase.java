c07b11a6aa113deb0476d931cb41c4b2283abbe9
TimzOwen <ktimothy@kabarak.ac.ke>
2020-05-08 15:58:28 +0300

    UI design

--- app/src/main/java/com/owen/note_keeper/NoteDatabase.java
+++ app/src/main/java/com/owen/note_keeper/NoteDatabase.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static android.provider.Contacts.SettingsColumns.KEY;
+
 public class NoteDatabase extends SQLiteOpenHelper {
 
     //create the table,dbName and dbVersion using finals.
@@ -25,10 +27,26 @@ public class NoteDatabase extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        //create table and pass in parameter and the key values.
+        //Create Table tableName(id INT primary Key,title TEXT, content TEXT,date TEXT, time TEXT)
+        String query = "CREATE TABLE " + DATABASE_TABLE + "("+KEY_ID+"INT PRIMARY KEY,"+
+                KEY_TITLE + "TEXT,"+
+                KEY_CONTENT + "TEXT,"+
+                KEY_DATE + "TEXT,"+
+                KEY_TIME + "TEXT" +")";
+        //We now execute the sql
+        db.execSQL(query);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        //check if there exist a new version of the db. otherwise drop table
+        if (oldVersion>=newVersion)
+            return;
+        db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+        onCreate(db);
 
     }
 }
